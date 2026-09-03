f6a1e478ec5f3942bd1dd722db47e2d32f350dc8
Supreet T <supsrasri@gmail.com>
2026-09-03 18:49:26 -0400

    updated construction pages

--- src/routes/ArchiveView.vue
+++ src/routes/ArchiveView.vue
@@ -1,13 +1,14 @@
 <script setup>
+import UnderConstruction from "../components/UnderConstruction.vue";
 
-const props = defineProps([])
-
+const props = defineProps([]);
 </script>
 
 <template>
-    <h1>This is the archive page</h1>
+  <UnderConstruction
+    title="Archive Page"
+    message="This page is under construction. Please check back soon."
+  />
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
