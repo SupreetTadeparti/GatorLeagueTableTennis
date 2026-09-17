33673b5120deb2a28245dc1b3cf6f84a33364555
Supreet T <supsrasri@gmail.com>
2026-09-17 16:47:49 -0400

    fixed nav bug

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -128,6 +128,7 @@ function closeMenu() {
   border-radius: 0.5em;
   box-shadow: 0 10px 35px rgba(0, 0, 0, 0.35);
   backdrop-filter: blur(16px);
+  z-index: 100;
 }
 
 .brand {
@@ -188,7 +189,6 @@ function closeMenu() {
   display: flex;
   align-items: center;
   gap: 1rem;
-  z-index: 100;
 }
 
 .nav-links {
